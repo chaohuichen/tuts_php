d4202f6420f68e1d0e0f00a07d30fde439f1e779
chaohuichen <chenpeter0821@gmail.com>
2020-07-05 13:41:30 -0400

    add more validation

--- add.php
+++ add.php
@@ -12,19 +12,31 @@ if (isset($_POST['submit'])) {
     if (empty($_POST['email'])) {
         echo "An email is require <br/>";
     } else {
-        echo htmlspecialchars($_POST['email']);
+        $email = $_POST['email'];
+        //take two params,var,type of filter build in php
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo 'email must be a valid email address';
+        }
     }
-
+    //check title
     if (empty($_POST['title'])) {
         echo "An title is require <br/>";
     } else {
-        echo htmlspecialchars($_POST['title']);
+        $title = $_POST['title'];
+        //reg match, reg expess, var 
+        if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+            echo 'Title must be letters and spaces only';
+        }
     }
-
+    //check ingredients
     if (empty($_POST['ingredients'])) {
         echo "At least one ingredient is require <br/>";
     } else {
-        echo htmlspecialchars($_POST['ingredients']);
+        $ingredients = $_POST['ingredients'];
+        //reg match, reg expess, var 
+        if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+            echo 'Ingredients must be a comma separeted list';
+        }
     }
 } //end of post check
 ?>
